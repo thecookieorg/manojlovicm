dabc2f2469979f541a9176fcc40ee5bf3a419103
Marko Manojlovic <marko@thecookieapps.com>
2016-04-02 00:49:31 +0000

    modified views for adding new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,8 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order("created_at desc")
+    @categories = Category.all
   end
 
   # GET /posts/1
